ddd1559adef0948a18d7996b3c88cad8a4238815
UIT.23520398 <23520398@gm.uit.edu.vn>
2024-11-11 18:29:29 +0700

    Update main and interface

--- src/button.cpp
+++ src/button.cpp
@@ -1,16 +1,38 @@
 #include "button.hpp"
 
-Button::Button(const char *imagePath, Vector2 imagePosition)
+Button::Button(const char *imagePath, Vector2 imagePosition, float scale)
 {
-    Texture=LoadTexture(imagePath);
+    Image image = LoadImage(imagePath);
+
+    int originalWidth = image.width;
+    int originalHeight = image.height;
+
+    int newWidth = static_cast<int>(originalWidth * scale);
+    int newHeight = static_cast<int>(originalHeight * scale);
+
+    ImageResize(&image, newWidth, newHeight);
+    texture = LoadTextureFromImage(image);
+    UnloadImage(image);
+    position = imagePosition;
 }
 
 Button::~Button()
 {
-    UnloadTexture(Texture);
+    UnloadTexture(texture);
 }
 
 void Button::Draw()
 {
-    DrawTexture(Texture, position.x, position.y, WHITE);
+    DrawTextureV(texture, position, WHITE);
+}
+
+bool Button::isPressed(Vector2 mousePos, bool mousePressed)
+{   
+    Rectangle rect = {position.x, position.y, static_cast<float>(texture.width), static_cast<float>(texture.height)};
+
+    if(CheckCollisionPointRec(mousePos,rect) && mousePressed)
+    {
+        return true;
+    }
+    return false;
 }
