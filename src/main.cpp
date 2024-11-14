763af9716ae370977528950ead90b7f914027ba7
UIT.23520353 <23520353@gm.uit.edu.vn>
2024-11-14 14:49:38 +0700

    Move global function to globalfunct.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,8 @@
-#include <raylib.h>
-#include <deque>
-#include <fstream>
-#include "raymath.h"
 #include "game.hpp"
 #include "button.hpp"
 #include "score.hpp"
 #include "frame.hpp"
 using namespace std;
-
-bool ElementInDeque(Vector2 element, deque<Vector2> deque)
-{
-    for(unsigned int i=0; i<deque.size(); i++)
-    {
-        if(Vector2Equals(deque[i], element))
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-bool EventTriggered(double interval)
-{
-    double currentTime=GetTime();
-    if(currentTime-lastUpdateTime>=interval)
-    {
-        lastUpdateTime=currentTime;
-        return true;
-    }
-    return false;
-}
 
 class Apple
 {
