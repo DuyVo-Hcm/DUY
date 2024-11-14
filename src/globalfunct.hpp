9a43371ce2061c0932ad14f624c68849126855ed
UIT.23520353 <23520353@gm.uit.edu.vn>
2024-11-14 15:59:46 +0700

    Update globalfunct.hpp

--- src/globalfunct.hpp
+++ src/globalfunct.hpp
@@ -2,7 +2,8 @@
 #include <deque>
 #include <fstream>
 #include "raymath.h"
+#include "globalvar.hpp"
 
-bool ElementInDeque(Vector2 element, deque<Vector2> deque);
+bool ElementInDeque(Vector2 element, std::deque<Vector2> deque);
 
 bool EventTriggered(double interval);
