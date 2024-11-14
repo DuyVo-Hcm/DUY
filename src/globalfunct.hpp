697e73bd931964da32418c9f2431ae7887a70885
UIT.23520353 <23520353@gm.uit.edu.vn>
2024-11-14 14:49:56 +0700

    Declare prototype

---
+++ src/globalfunct.hpp
@@ -0,0 +1,8 @@
+#include <raylib.h>
+#include <deque>
+#include <fstream>
+#include "raymath.h"
+
+bool ElementInDeque(Vector2 element, deque<Vector2> deque);
+
+bool EventTriggered(double interval);
